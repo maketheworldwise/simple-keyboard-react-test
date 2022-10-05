9bbd1b8da3474eb4c8db8f168ee01d1fca549059
Kevin Ahn <maketheworldwise@gmail.com>
2022-10-05 23:37:17 +0900

    :sparkles: 한글 키보드 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import { keyboardConfig } from "./keyboardConfig";
+import { HangulImeInputWrapper } from "mole-virtual-keyboard";
+
+import KeyboardReact from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
+
+import "./App.css";
+
+let inputWrapper = HangulImeInputWrapper;
 
 function App() {
+  const [isShift, setIsShift] = useState(false);
+
+  const inputRef = useRef(null);
+  useEffect(() => {
+    if (!inputRef.current) return;
+    inputWrapper = new HangulImeInputWrapper(inputRef.current);
+  }, []);
+
+  const onChange = (input) => {
+    // console.log("Input changed", input);
+  };
+
+  const onKeyPress = (button) => {
+    console.log("Button pressed", button);
+    if (button === "{shift}") {
+      setIsShift(!isShift);
+      return;
+    }
+
+    if (button === "{bksp}") {
+      inputWrapper?.backspace();
+    } else if (button === "{space}") {
+      inputWrapper?.insert(" ");
+    } else {
+      inputWrapper?.insert(button);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="keyboard_container">
+        <input
+          ref={inputRef}
+          type="text"
+          onSelect={() => {
+            inputWrapper?.checkChangedSelect();
+          }}
+        />
+        <KeyboardReact
+          layout={keyboardConfig.layout}
+          layoutName={isShift ? "shift" : "default"}
+          display={keyboardConfig.display}
+          onChange={onChange}
+          onKeyPress={onKeyPress}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/keyboardConfig.js
@@ -0,0 +1,24 @@
+export const keyboardConfig = {
+  layout: {
+    default: [
+      "1 2 3 4 5 6 7 8 9 0",
+      "ㅂ ㅈ ㄷ ㄱ ㅅ ㅛ ㅕ ㅑ ㅐ ㅔ",
+      "ㅁ ㄴ ㅇ ㄹ ㅎ ㅗ ㅓ ㅏ ㅣ",
+      "{shift} ㅋ ㅌ ㅊ ㅍ ㅠ ㅜ ㅡ {bksp}",
+      "123 {space} . {enter}",
+    ],
+    shift: [
+      "1 2 3 4 5 6 7 8 9 0",
+      "ㅃ ㅉ ㄸ ㄲ ㅆ ㅛ ㅕ ㅑ ㅐ ㅔ",
+      "ㅁ ㄴ ㅇ ㄹ ㅎ ㅗ ㅓ ㅏ ㅣ",
+      "{shift} ㅋ ㅌ ㅊ ㅍ ㅠ ㅜ ㅡ {bksp}",
+      "123 {space} . {enter}",
+    ],
+  },
+  display: {
+    "{bksp}": "backspace",
+    "{shift}": "shift",
+    "{enter}": "enter",
+    "{space}": "space",
+  },
+};
